672bb3b9cb92c9fef33f8be3a0104b303855ea3f
HamdiNawfel <hamdii.nawfel@gmail.com>
2022-02-04 22:54:02 +0100

    test deploy to heroku

---
+++ server.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("./frontend/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(port, () => {
+  console.log("Server is up!");
+});
